7988d694a170291cf747049ad25ca1bba67468bd
Akshat Shandilya <shandilya041@gmail.com>
2026-04-22 22:25:35 +0530

    Fixed CORS and updated API URL

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api', // Adjust in production
+  baseURL: 'https://o-nanh.onrender.com', // Adjust in production
   headers: {
     'Content-Type': 'application/json',
   },
